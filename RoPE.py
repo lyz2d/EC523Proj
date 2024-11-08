f6fc67e1c3f8a0e8d198004b2763682fd53b4e52
geyu <geyuok@hotmail.com>
2024-11-08 16:05:23 -0500

    Add some comments

--- RoPE.py
+++ RoPE.py
@@ -3,17 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 def precompute_freqs_cis(dim: int, seq_len: int, theta: float = 10000.0):
-    # 计算词向量元素两两分组之后，每组元素对应的旋转角度
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
-    # 生成 token 序列索引 t = [0, 1,..., seq_len-1]
+    # generate token  t = [0, 1,..., seq_len-1]
     t = torch.arange(seq_len, device=freqs.device)
     # freqs.shape = [seq_len, dim // 2] 
     freqs = torch.outer(t, freqs).float()
-    # torch.polar 的文档
+    # torch.polar documents
     # https://pytorch.org/docs/stable/generated/torch.polar.html
-    # 计算结果是个复数向量
-    # 假设 freqs = [x, y]
-    # 则 freqs_cis = [cos(x) + sin(x)i, cos(y) + sin(y)i]
+    # The result is a comlex vector
+    # if freqs = [x, y]
+    # then freqs_cis = [cos(x) + sin(x)i, cos(y) + sin(y)i]
     freqs_cis = torch.polar(torch.ones_like(freqs), freqs)
     return freqs_cis
 
@@ -27,12 +26,15 @@ def apply_rotary_emb(
     xq_ = xq.float().reshape(*xq.shape[:-1], -1, 2)
     xk_ = xk.float().reshape(*xk.shape[:-1], -1, 2)
     
-    # 转为复数域
+    # to complex
     xq_ = torch.view_as_complex(xq_)
     xk_ = torch.view_as_complex(xk_)
     
-    # 应用旋转操作，然后将结果转回实数域
+    # rotate and turn to real domain
     # xq_out.shape = [batch_size, seq_len, dim]
     xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(2)
     xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(2)
     return xq_out.type_as(xq), xk_out.type_as(xk)
+
+def laf_pos_embedding():
+    pass
